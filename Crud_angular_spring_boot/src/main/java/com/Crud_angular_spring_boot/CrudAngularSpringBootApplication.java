78687f8922eee0388a5bfb86093fdf924273864b
guismeiram <boralagui@outlook.com>
2022-06-21 02:13:27 -0300

    commitando projeto

---
+++ Crud_angular_spring_boot/src/main/java/com/Crud_angular_spring_boot/CrudAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Crud_angular_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAngularSpringBootApplication.class, args);
+	}
+
+}
---
+++ Crud_angular_spring_boot/src/test/java/com/Crud_angular_spring_boot/CrudAngularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Crud_angular_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAngularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
